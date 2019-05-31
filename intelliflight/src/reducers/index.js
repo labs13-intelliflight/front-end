229693a5da2941f58915ea13f79c89d86221d991
Mario Amaya <thefozcat@gmail.com>
2019-05-30 17:01:31 -0700

    updated redux boilerplate

---
+++ intelliflight/src/reducers/index.js
@@ -0,0 +1,14 @@
+// Set Initial State
+
+const initialState = {
+    users: []
+}
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
