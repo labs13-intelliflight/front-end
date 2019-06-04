5288f6efb8d276901273170e069a1277bfd44cbf
fixmylifedesigns <ijd.irving@gmail.com>
2019-06-04 12:18:46 -0400

    googlemap

--- intelliflight/src/App.js
+++ intelliflight/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import TestMap from "./components/map/map";
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Hi there!</h1> 
+      <div>Intelliflight map</div>
+      <TestMap />
       </div>
     </Router>
   );
---
+++ intelliflight/src/components/map/weather-icon.js
@@ -0,0 +1,39 @@
+import one from "../weather-icons/1.png";
+import two from "../weather-icons/2.png";
+import three from "../weather-icons/3.png";
+import four from "../weather-icons/4.png";
+import five from "../weather-icons/5.png";
+import six from "../weather-icons/6.png";
+import seven from "../weather-icons/7.png";
+import eight from "../weather-icons/8.png";
+import nine from "../weather-icons/9.png";
+import ten from "../weather-icons/10.png";
+import eleven from "../weather-icons/11.png";
+
+export default function weatherIcon(icon) {
+    if (icon == 1) {
+        return one
+      } if (icon == 2) {
+        return two
+      }if (icon == 3) {
+        return three
+      }if (icon == 4) {
+        return four
+      }if (icon == 5) {
+        return five
+      }if (icon == 6) {
+        return six
+      }if (icon == 7) {
+        return seven
+      }if (icon == 8) {
+        return eight
+      }if (icon == 9) {
+        return nine
+      }if (icon == 10) {
+        return ten
+      }if (icon == 11) {
+        return eleven
+      }else{
+        return null
+      }
+}
