6adf674a264c33219170267e95fbe2b357a4a799
Mario Amaya <thefozcat@gmail.com>
2019-06-04 12:43:25 -0700

    created new react app

--- intelliflight/src/App.js
+++ intelliflight/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import './App.css';
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Hi there!</h1> 
+        <h1>Hi there!</h1>
       </div>
     </Router>
   );
