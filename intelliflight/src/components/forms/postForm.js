b640949517b874f92e677f473304b00966db2a00
fixmylifedesigns <ijd.irving@gmail.com>
2019-06-13 17:36:11 -0400

    pirep form added

--- intelliflight/src/components/forms/postForm.js
+++ intelliflight/src/components/forms/postForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import WeatherIcon from "../map/functions/weather-icon.js";
 
 export default class PostForm extends Component {
   constructor(props) {
@@ -42,8 +43,13 @@ export default class PostForm extends Component {
   };
 
   render() {
+    console.log("turbulence:", this.state.turbulence );
+    console.log( "icing:", this.state.icing );
+    console.log("weather:", this.state.weather );
+
     return (
       <div>
+        <h1>Submit Pirep</h1>
         <form onSubmit={this.addPirep}>
           <div>
             <input
@@ -53,31 +59,273 @@ export default class PostForm extends Component {
               placeholder="altitude"
             />
           </div>
-          <div>
+          <div className="turbulence">
+            <h1>Turbulence</h1>
             <input
-              value={this.state.turbulence}
+              type="radio"
+              value="nil"
+              check={this.state.turbulence === "nil"}
               onChange={this.handleChange}
               name="turbulence"
               placeholder="turbulence"
             />
-          </div>
-          <div>
+            NIL
             <input
-              value={this.state.icing}
+              type="radio"
+              value="smooth-light"
+              check={this.state.turbulence === "smooth-light"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Smooth-Light
+            <input
+              type="radio"
+              value="light"
+              check={this.state.turbulence === "light"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Light
+            <input
+              type="radio"
+              value="light-moderate"
+              check={this.state.turbulence === "light-moderate"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Light-Moderate
+            <input
+              type="radio"
+              value="moderate"
+              check={this.state.turbulence === "moderate"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Moderate
+            <input
+              type="radio"
+              value="moderate-severe"
+              check={this.state.turbulence === "moderate-severe"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Moderate-Severe
+            <input
+              type="radio"
+              value="severe"
+              check={this.state.turbulence === "severe"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Severe
+            <input
+              type="radio"
+              value="extreme"
+              check={this.state.turbulence === "extreme"}
+              onChange={this.handleChange}
+              name="turbulence"
+              placeholder="turbulence"
+            />
+            Extreme
+          </div>
+          {/* ******************************************************************************************* */}
+          <div className="icing">
+            <h1>Icing</h1>
+            <input
+              type="radio"
+              value="nil"
+              check={this.state.icing === "nil"}
               onChange={this.handleChange}
               name="icing"
               placeholder="icing"
             />
-          </div>
-          <div>
+            NIL
             <input
-              value={this.state.weather}
+              type="radio"
+              value="trace"
+              check={this.state.icing === "trace"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Trace
+            <input
+              type="radio"
+              value="trace-light"
+              check={this.state.icing === "trace-light"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Trace-Light
+            <input
+              type="radio"
+              value="light"
+              check={this.state.icing === "light"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Light
+            <input
+              type="radio"
+              value="light-moderate"
+              check={this.state.icing === "light-moderate"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Light-Moderate
+            <input
+              type="radio"
+              value="moderate"
+              check={this.state.icing === "moderate"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Moderate
+            <input
+              type="radio"
+              value="moderate-severe"
+              check={this.state.icing === "moderate-severe"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Moderate-Severe
+            <input
+              type="radio"
+              value="severe"
+              check={this.state.icing === "severe"}
+              onChange={this.handleChange}
+              name="icing"
+              placeholder="icing"
+            />
+            Severe
+          </div>
+
+          {/* weather icons */}
+          <div className="weather">
+            <h1>Weather</h1>
+            <input
+              type="radio"
+              value="1"
+              check={this.state.weather === "1"}
               onChange={this.handleChange}
               name="weather"
               placeholder="weather"
             />
+            <img src={WeatherIcon(1)} alt="" />
+
+            <input
+              type="radio"
+              value="2"
+              check={this.state.weather === "2"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(2)} alt="" />
+
+            <input
+              type="radio"
+              value="3"
+              check={this.state.weather === "3"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(3)} alt="" />
+
+            <input
+              type="radio"
+              value="4"
+              check={this.state.weather === "4"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(4)} alt="" />
+
+            <input
+              type="radio"
+              value="5"
+              check={this.state.weather === "5"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(5)} alt="" />
+
+            <input
+              type="radio"
+              value="6"
+              check={this.state.weather === "6"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(6)} alt="" />
+
+            <input
+              type="radio"
+              value="7"
+              check={this.state.weather === "7"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(7)} alt="" />
+
+            <input
+              type="radio"
+              value="8"
+              check={this.state.weather === "8"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(8)} alt="" />
+
+            <input
+              type="radio"
+              value="9"
+              check={this.state.weather === "9"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(9)} alt="" />
+
+            <input
+              type="radio"
+              value="10"
+              check={this.state.weather === "10"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(10)} alt="" />
+
+            <input
+              type="radio"
+              value="11"
+              check={this.state.weather === "11"}
+              onChange={this.handleChange}
+              name="weather"
+              placeholder="weather"
+            />
+            <img src={WeatherIcon(11)} alt="" />
+
           </div>
-          <div>
+          <div className="description">
             <input
               value={this.state.description}
               onChange={this.handleChange}
@@ -85,7 +333,7 @@ export default class PostForm extends Component {
               placeholder="description"
             />
           </div>
-          <div>
+          <div className="latitude">
             <input
               value={this.state.latitude}
               onChange={this.handleChange}
@@ -93,7 +341,7 @@ export default class PostForm extends Component {
               placeholder="latitude"
             />
           </div>
-          <div>
+          <div className="longitude">
             <input
               value={this.state.longitude}
               onChange={this.handleChange}
