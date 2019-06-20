9a0f0fcd9df7a7f305e12405e7f42e046da472db
Mario Amaya <thefozcat@gmail.com>
2019-06-20 10:16:40 -0700

    added FlightPlanForm

---
+++ intelliflight/src/components/FlightPlanForm.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { GoogleComponent } from 'react-google-location';
+
+class FlightPlanForm extends React.Component {
+    
+    render() {
+        return (
+            <div className="top-content">
+                <h4>Plan Your Flight</h4>
+                <form onSubmit={this.props.submitFlightPlan}>
+                    <p>Starting Point</p>
+                    <GoogleComponent
+                        apiKey={process.env.REACT_APP_GOOGLE_KEY}
+                        language={'en'}
+                        coordinates={true}
+                        locationBoxStyle={'custom-style'}
+                        locationListStyle={'custom-style-list'}
+                        onChange={event => {
+                            localStorage.setItem('start', JSON.stringify(event))
+                        }}
+                    />
+                    <p>Destination</p>
+                    <GoogleComponent
+                        apiKey={process.env.REACT_APP_GOOGLE_KEY}
+                        language={'en'}
+                        coordinates={true}
+                        locationBoxStyle={'custom-style'}
+                        locationListStyle={'custom-style-list'}
+                        onChange={event => {
+                            localStorage.setItem('destination', JSON.stringify(event))
+                        }}
+                    />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default FlightPlanForm;
